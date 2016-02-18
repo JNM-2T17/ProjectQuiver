767db0006ea9afa0cd0e5829422bf2ee8e9fd040
Austin Fernandez <ryanaustinf@yahoo.com>
2016-02-18 11:25:25 +0800

    changes to jesus query

--- documentations/Best Per Category.sql
+++ documentations/Best Per Category.sql
@@ -1,16 +1,35 @@
-SELECT name, class, abstract, description, grade, recog
-FROM (SELECT recog, MAX(grade) AS grade
-		FROM (SELECT name, class, abstract, description, AVG(grade) AS grade, recog
-						FROM pq_project P INNER JOIN pq_project_grades PG 
-							ON P.id = PG.id AND P.status = 1 AND PG.status = 1
-						INNER JOIN pq_project_recogs PR 
-							ON P.id = PR.id
-						GROUP BY P.id)A
-		GROUP BY recog)A2
-		NATURAL JOIN 
-        (SELECT name, class, abstract, description, AVG(grade) AS grade, recog
-				FROM pq_project P INNER JOIN pq_project_grades PG 
-					ON P.id = PG.id AND P.status = 1 AND PG.status = 1
-				INNER JOIN pq_project_recogs PR 
-					ON P.id = PR.id
-				GROUP BY P.id)B
+SELECT id
+				FROM (SELECT recog, grade,dateAdded
+						FROM (SELECT recog,grade,MAX(A.dateAdded) dateAdded
+								FROM (SELECT name, class, abstract, description
+												, AVG(grade) AS grade, recog
+												, P.dateAdded
+										FROM pq_project P 
+										INNER JOIN pq_project_grades PG 
+											ON P.id = PG.id AND P.status = 1 
+												AND PG.status = 1
+										INNER JOIN pq_project_recogs PR 
+											ON P.id = PR.id
+										GROUP BY P.id) A
+						GROUP BY recog, grade) B NATURAL JOIN 
+						(SELECT recog,MAX(grade) as grade
+							FROM (SELECT name, class, abstract, description
+											, AVG(grade) AS grade, recog
+											, P.dateAdded
+									FROM pq_project P 
+									INNER JOIN pq_project_grades PG 
+										ON P.id = PG.id AND P.status = 1 
+											AND PG.status = 1
+									INNER JOIN pq_project_recogs PR 
+										ON P.id = PR.id
+									GROUP BY P.id) A
+							GROUP BY recog)C)A2
+						NATURAL JOIN 
+				        (SELECT P.id,AVG(grade) AS grade, recog, P.dateAdded
+								FROM pq_project P 
+								INNER JOIN pq_project_grades PG 
+									ON P.id = PG.id AND P.status = 1 
+										AND PG.status = 1
+								INNER JOIN pq_project_recogs PR 
+									ON P.id = PR.id
+								GROUP BY P.id)B
